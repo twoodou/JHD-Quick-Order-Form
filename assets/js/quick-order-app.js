74d5486c669f49c4d7c5e92507588f0797ca7bfe
twood <twoodou@outlook.com>
2018-07-17 09:26:57 -0500

    transferred styling to external stylesheet, added color lookup functionality to external js (quick-order-app.js)

--- assets/js/quick-order-app.js
+++ assets/js/quick-order-app.js
@@ -23,14 +23,158 @@ $(document).ready(function () {
                 PColorCalculated = jsonData.Products[i].productname.split(" - ");
                 console.log(PColorCalculated.length);
                 if (PColorCalculated.length === 2) {
-                    $("#input-color").val(PColorCalculated[1]).prop("disabled", true);
+                    $("#input-color").empty();
+                    // $("#input-color").append($('<option>', {
+                    //     value: PColorCalculated[1]
+                    // }));
+                    $('#input-color').append('<option value="1">' + PColorCalculated[1] + '</option>');
+                    $("#input-color").prop("disabled", true);
                 } else {
                     $("#input-color").val("").prop("disabled", false);
+                    lookupColors(itemName);
                 }
             }
         }
         lookupMonoStyles(itemName);
         lookupSymbols(itemName);
+    }
+
+    function lookupColors(itemName) {
+        $.getJSON("./assets/json/Colors.json", function (data) {
+            var womensLeatherColorsArray = ["Lola", "King's Pad", "Everyday Tote", "Britton Backpack"];
+            var allLeatherColorsArray = ["Passport Cover", "Kenedy", "Clear Mini Makeup Case", "Clear Becky", "Airport Chico", "Luggage Tag", "Game Day Tote", "Clear Grande", "Large Laundry Bag"];
+            var bocaChicaColorsArray = ["Boca Chica"];
+            var huntingItemColorsArray = ["Bird Bag", "Sidekick", "Shotgun Cover", "Rifle Cover", "Small Revolver Case", "Large Revolver Case"];
+            var clearBackpackColorsArray = ["Clear Backpack"];
+            var JHDoppKitColorsArray = ["JH Dopp Kit"];
+            var bridleMahoganyColorsArray = ["McClip"];
+            var highlandParkToteColorsArray = ["Highland Park Tote"];
+            var sandPrintedColorsArray = ["Diaper Pad"];
+            var blackPrintedColorsArray = ["Makeup Case Organizer"];
+            var splitItemName = itemName.split(" - ")[0];
+
+            if (_.contains(womensLeatherColorsArray, splitItemName)) {
+                console.log("Women's Leather Colors");
+                colors = data.Colors[0].womensLeatherColors;
+                $("#input-color").empty();
+                $.each(colors, function (index, value) {
+                    $("#input-color").append($('<option>', {
+                        value: value,
+                        text: index
+                    }));
+                });
+            }
+            else if (_.contains(allLeatherColorsArray, splitItemName)) {
+                console.log("All Leather Colors");
+                colors = data.Colors[0].leatherColors;
+                $("#input-color").empty();
+                $.each(colors, function (index, value) {
+                    $("#input-color").append($('<option>', {
+                        value: value,
+                        text: index
+                    }));
+                });
+            }
+            else if (_.contains(bocaChicaColorsArray, splitItemName)) {
+                console.log("Boca Chica Colors");
+                colors = data.Colors[0].bocaChicaColors;
+                $("#input-color").empty();
+                $.each(colors, function (index, value) {
+                    $("#input-color").append($('<option>', {
+                        value: value,
+                        text: index
+                    }));
+                });
+            }
+            else if (_.contains(huntingItemColorsArray, splitItemName)) {
+                console.log("Hunting Colors");
+                colors = data.Colors[0].huntingItemColors;
+                $("#input-color").empty();
+                $.each(colors, function (index, value) {
+                    $("#input-color").append($('<option>', {
+                        value: value,
+                        text: index
+                    }));
+                });
+            }
+            else if (_.contains(clearBackpackColorsArray, splitItemName)) {
+                console.log("Clear Backpack Colors");
+                colors = data.Colors[0].clearBackpackColors;
+                $("#input-color").empty();
+                $.each(colors, function (index, value) {
+                    $("#input-color").append($('<option>', {
+                        value: value,
+                        text: index
+                    }));
+                });
+            }
+            else if (_.contains(JHDoppKitColorsArray, splitItemName)) {
+                console.log("JH Dopp Kit Colors");
+                colors = data.Colors[0].JHDoppKitColorsLeather;
+                $("#input-color").empty();
+                $.each(colors, function (index, value) {
+                    $("#input-color").append($('<option>', {
+                        value: value,
+                        text: index
+                    }));
+                });
+            }
+            else if (_.contains(bridleMahoganyColorsArray, splitItemName)) {
+                console.log("Bridle or Mahogany");
+                colors = data.Colors[0].bridleMahogany;
+                $("#input-color").empty();
+                $.each(colors, function (index, value) {
+                    $("#input-color").append($('<option>', {
+                        value: value,
+                        text: index
+                    }));
+                });
+            }
+            else if (_.contains(highlandParkToteColorsArray, splitItemName)) {
+                console.log("Highland Park Tote Colors");
+                colors = data.Colors[0].highlandParkToteColors;
+                $("#input-color").empty();
+                $.each(colors, function (index, value) {
+                    $("#input-color").append($('<option>', {
+                        value: value,
+                        text: index
+                    }));
+                });
+            }
+            else if (_.contains(sandPrintedColorsArray, splitItemName)) {
+                console.log("Sand Printed Lining");
+                colors = data.Colors[0].sandPrintedLiningColor;
+                $("#input-color").empty();
+                $.each(colors, function (index, value) {
+                    $("#input-color").append($('<option>', {
+                        value: value,
+                        text: index
+                    }));
+                });
+            }
+            else if (_.contains(blackPrintedColorsArray, splitItemName)) {
+                console.log("Black Printed Lining");
+                colors = data.Colors[0].blackLiningColor;
+                $("#input-color").empty();
+                $.each(colors, function (index, value) {
+                    $("#input-color").append($('<option>', {
+                        value: value,
+                        text: index
+                    }));
+                });
+            }
+            else {
+                console.log("All Vinyl Colors");
+                colors = data.Colors[0].vinylColors;
+                $("#input-color").empty();
+                $.each(colors, function (index, value) {
+                    $("#input-color").append($('<option>', {
+                        value: value,
+                        text: index
+                    }));
+                });
+            }
+        });
     }
 
     function lookupMonoStyles(itemName) {
@@ -136,13 +280,26 @@ $(document).ready(function () {
             availableProducts.push(data.Products[i].productname);
         }
         $("#input-item").autocomplete({
-            source: availableProducts
+            source: availableProducts,
+            select: function( event, ui ) {
+            }
         });
     });
 
-    $(document).on('change blur keypress', '#input-item', function () {
+    // $(document).on('autocompleteselect change blur keypress keyup click', '#input-item', function () {
+    //     var itemName = $(this).val();
+    //     lookupItemCode(itemName);
+    // });
+
+    $("#input-item").on("autocompleteselect", function( event, ui ) {
+        // event.preventDefault();
+        console.log(ui);
         var itemName = $(this).val();
         lookupItemCode(itemName);
     });
+
+    // $('html').bind('#input-item', function() {
+    //     alert('test');
+    // });
 
 });
